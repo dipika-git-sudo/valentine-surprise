382ee9fe31c0400223874d880272123ad18adcd3
dipika-git-sudo <dipikamaji09@gmail.com>
2026-02-09 12:52:45 +0530

    Add WhiteNoise + STATIC_ROOT

--- webvalen/settings.py
+++ webvalen/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-r@ps$)&-s600caf&byni46olvh+qr&67e_4p=asa+ntb(989w0"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = "True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]  # Change this to your domain in production
 
 
 # Application definition
@@ -42,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -118,4 +120,7 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
-SITE_PASSCODE = "07122025"  # Change this to your desired passcode
+STATIC_ROOT = BASE_DIR / "staticfiles"
+SITE_PASSCODE = os.getenv(
+    "SITE_PASSCODE", "07122025"
+)  # Change this to your desired passcode
